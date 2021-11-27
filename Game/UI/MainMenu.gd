09d6c71c69ef32f2c94aedb02e041fd33420061a
bogi000 <bogdanjsd@gmail.com>
2021-11-27 23:31:43 +0100

    made game into 2 separate folders

--- Game/UI/MainMenu.gd
+++ Game/UI/MainMenu.gd
@@ -9,3 +9,6 @@ func _process(delta):
 	var screen_size = get_viewport_rect().size
 	var mpos = get_viewport().get_mouse_position()
 	cc.rect_position = screen_size/2 - (screen_size/2-mpos)/16.0 + menu_offset
+	for button in cc.get_node("VBoxContainer").get_children():
+		var new_scale = 1.0 + int(button.get_global_rect().has_point(get_global_mouse_position()))*0.4
+		cc.rect_scale = lerp(cc.rect_scale,Vector2(new_scale,new_scale),10*delta)

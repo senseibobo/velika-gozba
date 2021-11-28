44814511be74a8bc12bd381382722d19b871f1cd
bogi000 <bogdanjsd@gmail.com>
2021-11-28 20:37:07 +0100

    added new enemies

--- Game/Enemies/Enemy.gd
+++ Game/Enemies/Enemy.gd
@@ -1,6 +1,8 @@
 extends Character
 class_name Enemy
 
+export var death_particles_scene : PackedScene
+
 onready var sprite : Sprite = get_node("Sprite")
 
 func _ready():
@@ -21,5 +23,10 @@ func hit_flash():
 
 func death(source):
 	.death(source)
+	if death_particles_scene != null:
+		var particles = death_particles_scene.instance()
+		get_parent().add_child(particles)
+		particles.emitting = true
+		particles.global_position = global_position
 	queue_free()
 	Global.enemies.erase(self)

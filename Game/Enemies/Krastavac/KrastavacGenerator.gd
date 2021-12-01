48f3fdce17c008b6c2615db7d2fec1e988b5b6da
Toda00 <bilosta2004@gmail.com>
2021-12-01 11:59:45 +0100

    Updated Krastavac

--- Game/Enemies/Krastavac/KrastavacGenerator.gd
+++ Game/Enemies/Krastavac/KrastavacGenerator.gd
@@ -1,17 +1,17 @@
 extends BulletGenerator
 
 
-onready var krastavac = get_parent()
-onready var sprite = krastavac.get_node("Sprite")
-onready var puskapos = sprite.get_node("Puska/PuskaPos")
+onready var krastavac : Node = get_parent()
+onready var sprite : Sprite = krastavac.get_node("Sprite")
+onready var puskapos : Node = sprite.get_node("Puska/PuskaPos")
 
-func shoot():
+func shoot() -> void:
 	if not is_instance_valid(Global.player): return
 	global_position = puskapos.global_position
-	var bullet = Bullet.new()
+	var bullet : Bullet = Bullet.new()
 	bullet.source = krastavac
 	bullet.position = global_position
-	var dir = sign(Global.player.global_position.x-krastavac.global_position.x)
+	var dir : int = sign(Global.player.global_position.x-krastavac.global_position.x)
 	bullet.rotation = Vector2(dir,0).angle()
 	bullet.velocity = Vector2(dir*speed,0)
 	add_bullet(bullet)
